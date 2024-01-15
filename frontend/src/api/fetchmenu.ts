df61dcd46b9f40842552c3c3085aa146c69bad84
Frosty <nayanbastola777@gmail.com>
2024-01-14 23:31:27 -0500

    Re final push

--- frontend/src/api/fetchmenu.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchMenuData(): Promise<any[]> {
-  const response = await fetch(`${import.meta.env.VITE_COREAPI_URL}/api/bom`);
-  const data = await response.json();
-  return data;
-}
