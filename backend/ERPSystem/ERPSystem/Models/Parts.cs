1ff9879cada4432fd28f97671a817d0b7a036b5e
Frosty <nayanbastola777@gmail.com>
2024-01-14 23:33:26 -0500

    Add gitignore

---
+++ backend/ERPSystem/ERPSystem/Models/Parts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Models
+{
+    public class Parts
+    {
+        public string NAME { get; set; }
+        public string TYPE { get; set; }
+        public string ITEM { get; set; }
+        public string PART_NUMBER { get; set; }
+        public string TITLE { get; set; }
+        public string MATERIAL { get; set; }
+    }
+}
